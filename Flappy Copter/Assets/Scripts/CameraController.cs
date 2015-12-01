b65bcf15f0d86c6888f0d0b7cd70a03768fcb1c4
Matilda Ericsson <tilda.ericson@gmail.com>
2015-12-01 17:07:21 +0100

    alla filer

---
+++ Flappy Copter/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject helicopter = GameObject.FindGameObjectWithTag("helicopterTag");
+        float helicopterX = helicopter.transform.position.x;
+        float helicopterY = helicopter.transform.position.y;
+        transform.position = new Vector3(helicopterX, helicopterY, transform.position.z);
+	}
+    
+}
---
+++ Flappy Copter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float power;
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            //rb.AddForce(transform.up * power, ForceMode.Impulse);
+            rb.velocity = new Vector3(speed, power, rb.velocity.z);
+        }
+
+        rb.velocity = new Vector3(speed, rb.velocity.y, rb.velocity.z);
+    }
+}
